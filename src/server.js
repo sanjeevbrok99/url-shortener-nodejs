72922f3efe685948db2a3e332e98dbb2b0bfd450
Sanjeev Mishra <mishragsanjeev1999@gmail.com>
2023-02-04 20:22:01 +0530

    server setup done

---
+++ src/server.js
@@ -0,0 +1,17 @@
+import express from 'express';
+import dotenv from "dotenv";
+dotenv.config({ path: "./config/config.env" });
+import connectDB from "./config/db.js";
+import logger from "morgan";
+const app = Express();
+
+connectDB();
+app.use(express.json({ extended: false }));
+// Server Setup
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log(
+        `API is listening on PORT: ${PORT} in ${process.env.NODE_ENV} mode`
+          .underline.green.bold
+      );
+});
